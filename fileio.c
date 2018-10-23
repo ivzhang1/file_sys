cdefbc7ef1e5249374ba69c963eb9a1aa5153862
ivzhang1 <izhang1@stuy.edu>
2018-10-23 11:10:31 -0400

    started

--- fileio.c
+++ fileio.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int main(){
   //open, close, read,write
 
-  
+  int file_id = open("text_file.txt");
+  printf("%d\n", file_id);
 
   
 }
