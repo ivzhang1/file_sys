78ce777399227738a3219e2e535678b259941045
ivzhang1 <izhang1@stuy.edu>
2018-10-23 22:09:52 -0400

    open, read works

--- fileio.c
+++ fileio.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(){
   //open, close, read,write
-
-  int file_id = open("text_file.txt");
+  char * file = "text_file.txt";
+  int file_id = open(file, O_RDWR); //READ ONLY
+  char * k = malloc(10);
   printf("%d\n", file_id);
-
   
+  printf("%zd\n", read(file_id, k, 20));
+  printf("%s\n", k);
+  
+  return close(file_id);
 }
