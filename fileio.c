c94e2665e813038872f3d11cac19d7bdb05b2c02
ivzhang1 <izhang1@stuy.edu>
2018-10-26 19:43:46 -0400

    added fancy output

--- fileio.c
+++ fileio.c
@@ -8,14 +8,14 @@ int main(){
   char * file = "text_file.txt";
   int file_id = open(file, O_RDWR); //READ ONLY
   char * k = malloc(10);
-  printf("%d\n", file_id);
+  printf("File Id: %d\n", file_id);
   
-  printf("%zd\n", read(file_id, k, 20));
-  printf("%s\n", k);
+  printf("Bytes Read: %zd\n", read(file_id, k, 20));
+  printf("String read: %s\n", k);
 
 
   int write_size = write(file_id, "woad\n", 5);
-  printf("%d\n", write_size);
+  printf("Write Size: %d\n", write_size);
 
   return close(file_id);
 }
