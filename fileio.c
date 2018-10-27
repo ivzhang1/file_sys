03367a9cd5dd6613ca885dd806fe990767aa43dd
simont1 <88simont@gmail.com>
2018-10-27 02:16:04 -0400

    s

--- fileio.c
+++ fileio.c
@@ -6,10 +6,10 @@
 int main(){
   //open, close, read,write
   char * file = "text_file.txt";
-  int file_id = open(file, O_RDWR); //READ ONLY
+  int file_id = open(file, O_RDWR); //READ AND WRITE
   char * k = malloc(10);
   printf("File Id: %d\n", file_id);
-  
+
   printf("Bytes Read: %zd\n", read(file_id, k, 20));
   printf("String read: %s\n", k);
 
@@ -17,5 +17,11 @@ int main(){
   int write_size = write(file_id, "woad\n", 5);
   printf("Write Size: %d\n", write_size);
 
+  char file2 = open("test2.txt", O_RDONLY);
+  char info[50];
+  read(file2, info, 50);
+  printf("%s\n", info);
+  close(file2);
   return close(file_id);
+
 }
